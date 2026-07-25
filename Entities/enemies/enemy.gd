68566e7ffad5a94a090aa72debed95c8c9afc50c
himanshusapkota <himanshuspkt@gmail.com>
2026-07-25 23:45:03 +0545

    Final Commit

--- Entities/enemies/enemy.gd
+++ Entities/enemies/enemy.gd
@@ -1,11 +1,21 @@
 class_name Enemy
 extends Area2D
 
-var speed: int = 80
+var speed: float = 80.0
 
 func _ready() -> void:
 	randomize()
 	position = Vector2(randi_range(0, 400), 0)
 
-func _process(delta):
+	# Connect the collision signal
+	area_entered.connect(_on_area_entered)
+
+func _process(delta: float) -> void:
 	position.y += speed * delta
+
+func _on_area_entered(area: Area2D) -> void:
+	
+
+	if area.name == "BulletArea":
+		area.get_parent().queue_free() 
+		queue_free()                  
--- Entities/enemy_spawner/enemy_spawner.gd
+++ Entities/enemy_spawner/enemy_spawner.gd
@@ -3,6 +3,8 @@ extends Node
 var enemy_scene = preload("res://Entities/enemies/enemy.tscn")
 
 func _ready() -> void:
+	get_parent().get_node("Boundary").area_entered.connect(_the_end)
+
 	var timer = Timer.new()
 	add_child(timer)
 
@@ -13,3 +15,11 @@ func _ready() -> void:
 func _create_enemy():
 	var enemy = enemy_scene.instantiate()
 	get_parent().get_node("Enemies").add_child(enemy)
+
+func _the_end(body):
+	if body is Enemy:
+		get_tree().paused = true
+		
+	
+		
+	
