9401f3fc5771d1591a1b66f4074c499caa055df8
himanshusapkota <himanshuspkt@gmail.com>
2026-07-25 22:14:17 +0545

    Solved Error

--- Entities/Player/player.gd
+++ Entities/Player/player.gd
@@ -1,12 +1,23 @@
 extends Node2D
 
+var bullet_scene = preload("res://Entities/Bullet/bullet.tscn")
 var direction: int = 0
 var speed: int = 300
+var shoot_time: float = 1.0
+var shoot_counter: float = 0.0
+
 func _ready():
-	print("Player script is ready!")
+	pass
 
 func _process(delta):
-	print("process running, direction=", direction)
+	shoot_counter += delta
+	if Input.is_action_pressed("shoot") and shoot_counter > shoot_time:
+		shoot_counter = 0
+		var bullet_instance = bullet_scene.instantiate()
+		bullet_instance.position.x = position.x
+		bullet_instance.position.y = position.y - 50
+		get_tree().current_scene.get_node("Bullets").add_child(bullet_instance)
+
 	if Input.is_action_pressed("left"):
 		direction = -1
 	elif Input.is_action_pressed("right"):
@@ -15,9 +26,4 @@ func _process(delta):
 		direction = 0
 
 	position.x += direction * speed * delta
-	position.x=clamp(position.x,15,385)
-	
-
-	
-	
-	
+	position.x = clamp(position.x, 15, 385)
--- player.gd
+++ player.gd
@@ -1,14 +1,11 @@
 extends Node2D
-
 var bullet_scene = preload("res://Entities/Bullet/bullet.tscn")
 var direction: int = 0
 var speed: int = 300
 var shoot_time: float = 1.0
 var shoot_counter: float = 0.0
-
 func _ready():
-	pass
-
+	print("Player script is ready!")
 func _process(delta):
 	shoot_counter += delta
 	if Input.is_action_pressed("shoot") and shoot_counter > shoot_time:
@@ -17,13 +14,11 @@ func _process(delta):
 		bullet_instance.position.x = position.x
 		bullet_instance.position.y = position.y - 50
 		get_tree().current_scene.get_node("Bullets").add_child(bullet_instance)
-
 	if Input.is_action_pressed("left"):
 		direction = -1
 	elif Input.is_action_pressed("right"):
 		direction = 1
 	else:
 		direction = 0
-
 	position.x += direction * speed * delta
 	position.x = clamp(position.x, 15, 385)
