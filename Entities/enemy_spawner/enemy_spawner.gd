c036a96215021407c6ba4b2c1c3133fb687a7918
himanshusapkota <himanshuspkt@gmail.com>
2026-07-26 10:19:03 +0545

    Added Buttons

--- Entities/enemy_spawner/enemy_spawner.gd
+++ Entities/enemy_spawner/enemy_spawner.gd
@@ -5,7 +5,7 @@ var enemy_scene = preload("res://Entities/enemies/enemy.tscn")
 func _ready() -> void:
 	get_parent().get_node("Boundary").area_entered.connect(_the_end)
 
-	var timer = Timer.new()
+	var timer := Timer.new()
 	add_child(timer)
 
 	timer.wait_time = 1.5
@@ -17,9 +17,7 @@ func _create_enemy():
 	get_parent().get_node("Enemies").add_child(enemy)
 
 func _the_end(body):
+	print("Something entered the boundary: ", body.name)
 	if body is Enemy:
 		get_tree().paused = true
-		
-	
-		
-	
+		get_parent().get_node("GameOverUI").show_game_over()
---
+++ game_over_ui.gd
@@ -0,0 +1,11 @@
+extends CanvasLayer
+
+func _ready() :
+	hide()
+
+func show_game_over() :
+	print("Game Over UI shown")
+	show()
+
+
+	
